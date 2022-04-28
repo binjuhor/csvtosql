b195f230d6e81eb328e1133991f97ce64f6078cd
Hoang Kiem <kiemhd@outlook.com>
2022-04-28 20:51:13 +0700

    Add SQL create table

--- index.php
+++ index.php
@@ -8,40 +8,52 @@ $columnName = getColumnName($arrayData);
 $createTable = createSQLTable($columnName);
 
 
-function createSQLTable( $columns, $tableName = 'myTable' ) {
+function createSQLTable($columns, $tableName = 'myTable')
+{
     $sql = "CREATE TABLE IF NOT EXISTS `{$tableName}` (`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,";
-    foreach($columns as $column) {
+    foreach ($columns as $column) {
         $sql .= "`{$column}` varchar(255) NOT NULL,";
     }
     return substr($sql, 0, -1) . ") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 }
 
-function renameDuplicateColumn( $columns ) {
-
-    foreach($columns as $key => $value) {
-        if(array_count_values($columns) > 1) {
-            $columns[$key] = $value . '_';
+function getColumnName($data)
+{
+    $columnList = $data[0];
+    $duplicateArray = duplicateColumn($columnList);
+    $i = 0;
+    
+    foreach($columnList as $key => $column) {
+        if(in_array($column, $duplicateArray) && $key > 0) {
+            $columnList[$key] = $column . '_' . $i;
+            $i++;
         }
     }
 
-    return $columns;
+    return $columnList;
 }
 
-function getColumnName( $data ){
-    return $data[0];
+function duplicateColumn( $columns ) 
+{
+    $duplicate = [];
+    $array = array_count_values($columns);
+    foreach($array as $key => $arr) {
+        if($arr > 1) {
+            $duplicate[] =  $key;
+        }
+    }
+
+    return $duplicate;
 }
 
 function csvToArray($csvFile)
 {
     if (($fileOpen = fopen($csvFile, "r")) !== FALSE) {
-
         while (($data = fgetcsv($fileOpen, 1000, ";")) !== FALSE) {
             $csvData[] = $data;
         }
-
         fclose($fileOpen);
     }
-
     return $csvData;
 }
 
